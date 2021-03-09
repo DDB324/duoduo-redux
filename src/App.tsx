d8c6366892a203ad9fff22db6f31dfa73e59b732
ddb324 <jiangdaoran@163.com>
2021-03-09 19:49:12 +0800

    引入antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import 'antd/dist/antd.css'
+import {Input,Button,List} from 'antd'
+
+const data = [
+  '早上10点面试',
+  '下午2点面试'
+]
 
 function App() {
   return (
-    <div className="App">
-    Hello World
+    <div style={{margin:'10px'}}>
+    <Input placeholder='写点什么' style={{width:'250px',marginRight:'10px'}} />
+    <Button type='primary'>增加</Button>
+      <div style={{margin:'10px',width:'300px'}}>
+        <List
+        bordered
+        dataSource={data}
+        renderItem={item=><List.Item>{item}</List.Item>}
+        />
+      </div>
     </div>
   );
 }
