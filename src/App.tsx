2296aca63f4a0ba99ae69fe1bb4992865a325ba6
ddb324 <jiangdaoran@163.com>
2021-03-09 20:54:31 +0800

    获取并展示数据

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
 import React from 'react';
 import 'antd/dist/antd.css'
 import {Input,Button,List} from 'antd'
-
-const data = [
-  '早上10点面试',
-  '下午2点面试'
-]
+import {store} from './store'
 
 function App() {
+  const state = store.getState()
   return (
     <div style={{margin:'10px'}}>
-    <Input placeholder='写点什么' style={{width:'250px',marginRight:'10px'}} />
+    <Input placeholder={state.inputValue}
+           style={{width:'250px',marginRight:'10px'}} />
     <Button type='primary'>增加</Button>
       <div style={{margin:'10px',width:'300px'}}>
         <List
         bordered
-        dataSource={data}
+        dataSource={state.list}
         renderItem={item=><List.Item>{item}</List.Item>}
         />
       </div>
--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,6 +1,12 @@
-const defaultState = {};
+const defaultState = {
+  inputValue: '写点什么',
+  list: [
+    '早上10点面试',
+    '下午2点面试'
+  ]
+};
 
-const reducer = (state = defaultState, action) => {
+const reducer = (state = defaultState, action:any) => {
   return state;
 };
 
