208539a577e991bcadb108b205e1aa3ab87a7c27
ddb324 <jiangdaoran@163.com>
2021-03-11 22:57:53 +0800

    使用redux-dev-tools调试工具

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,14 @@
 import {createStore} from 'redux'
 import {reducer} from './reducer';
 
-const store = createStore(reducer)
+//TS扩展全局变量
+declare global {
+  interface Window { __REDUX_DEVTOOLS_EXTENSION__: any; }
+}
+
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 
 export {store}
