0b43314dc7f74d945146839eadf9e00aa9c0f07d
ddb324 <jiangdaoran@163.com>
2021-03-12 22:33:53 +0800

    联系input的内容与state内容

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import 'antd/dist/antd.css'
-import {Input,Button,List} from 'antd'
-import {store} from './store'
+import React, {ChangeEvent, useState} from 'react';
+import 'antd/dist/antd.css';
+import {Input, Button, List} from 'antd';
+import {store} from './store';
 
-function App() {
-  const state = store.getState()
+const App: React.FC = () => {
+  const [state, setState] = useState(store.getState());
+
+  //订阅store,更新的时候调用setState来更新UI
+  store.subscribe(() => setState(store.getState()));
+
+  //用dispatch来传入操作和值
+  const changeInputValue = (e: ChangeEvent<HTMLInputElement>) => {
+    const action = {
+      type: 'changeInput',
+      value: e.target.value
+    };
+    store.dispatch(action);
+  };
+
+  //受控input组件,value和onChange事件
   return (
-    <div style={{margin:'10px'}}>
-    <Input placeholder={state.inputValue}
-           style={{width:'250px',marginRight:'10px'}} />
-    <Button type='primary'>增加</Button>
-      <div style={{margin:'10px',width:'300px'}}>
+    <div style={{margin: '10px'}}>
+      <Input placeholder={state.inputPlaceHolder}
+             style={{width: '250px', marginRight: '10px'}}
+             onChange={changeInputValue}
+             value={state.inputValue}
+      />
+      <Button type='primary'>增加</Button>
+      <div style={{margin: '10px', width: '300px'}}>
         <List
-        bordered
-        dataSource={state.list}
-        renderItem={item=><List.Item>{item}</List.Item>}
+          bordered
+          dataSource={state.list}
+          renderItem={item => <List.Item>{item}</List.Item>}
         />
       </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,12 +1,28 @@
-const defaultState = {
-  inputValue: '写点什么',
+type State = {
+  inputValue: string
+  inputPlaceHolder: string
+  list: string[]
+}
+type Action = {
+  type: string
+  value: string
+}
+
+const defaultState: State = {
+  inputValue: '',
+  inputPlaceHolder: '写点什么',
   list: [
     '早上10点面试',
     '下午2点面试'
   ]
 };
 
-const reducer = (state = defaultState, action:any) => {
+const reducer = (state = defaultState, action: Action) => {
+  if (action.type === 'changeInput') {
+    let newState = JSON.parse(JSON.stringify(state));
+    newState.inputValue = action.value;
+    return newState;
+  }
   return state;
 };
 
