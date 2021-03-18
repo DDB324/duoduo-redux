3495b6de8e4b7f2776c363b4f671b48f003f08ed
ddb324 <jiangdaoran@163.com>
2021-03-18 23:23:44 +0800

    执行2个中间件

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,20 @@
-import {createStore} from 'redux'
+import {createStore, applyMiddleware, compose} from 'redux';
 import {reducer} from './reducer';
+import thunk from 'redux-thunk';
 
 //TS扩展全局变量
 declare global {
-  interface Window { __REDUX_DEVTOOLS_EXTENSION__: any; }
+  interface Window {__REDUX_DEVTOOLS_EXTENSION_COMPOSE__: any;}
 }
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__?
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}):compose
+
+const enhancer = composeEnhancers(applyMiddleware(thunk))
 
 const store = createStore(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  enhancer
+);
 
-export {store}
+export {store};
