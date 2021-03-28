d0114d9d139358724458affbd4b512f162d42bc9
ddb324 <jiangdaoran@163.com>
2021-03-28 14:24:43 +0800

    增加和删除list的内容

--- src/TodoListUI2.tsx
+++ src/TodoListUI2.tsx
@@ -1,29 +1,45 @@
 import React, {ChangeEvent} from 'react';
 import {connect} from 'react-redux';
 import {State} from './store/reducer';
-import {changeInputAction} from './store/actionCreator';
+import {changeInputAction, clickBtnAction, deleteItemAction} from './store/actionCreator';
 
 type Props = {
   inputPlaceHolder?: string
   inputChange?: (e: ChangeEvent<HTMLInputElement>) => void
+  clickBtn?: () => void
+  inputValue?: string
+  list: string[]
+  deleteItem?: (index: number) => void
 }
 
 export const TodoListUI2: React.FC<Props> = (props) => {
-  const {inputPlaceHolder, inputChange} = props;
+  const {inputPlaceHolder, inputValue, list, inputChange, clickBtn, deleteItem} = props;
   return (
     <div>
       <input type="text"
              placeholder={inputPlaceHolder}
              onChange={inputChange}
+             value={inputValue}
       />
-      <button>提交</button>
+      <button onClick={clickBtn}>提交</button>
+      <ul>
+        {
+          list.map((item, index) => {
+            return (
+              <li key={index} onClick={()=>deleteItem?.(index)}>{item}</li>
+            );
+          })
+        }
+      </ul>
     </div>
   );
 };
 
 const stateToProps = (state: State) => {
   return {
-    inputPlaceHolder: state.inputPlaceHolder
+    inputPlaceHolder: state.inputPlaceHolder,
+    inputValue: state.inputValue,
+    list: state.list
   };
 };
 
@@ -31,6 +47,12 @@ const dispatchToProps = (dispatch: any) => {
   return {
     inputChange(e: ChangeEvent<HTMLInputElement>) {
       dispatch(changeInputAction(e.target.value));
+    },
+    clickBtn() {
+      dispatch(clickBtnAction());
+    },
+    deleteItem(index: number) {
+      dispatch(deleteItemAction(index));
     }
   };
 };
